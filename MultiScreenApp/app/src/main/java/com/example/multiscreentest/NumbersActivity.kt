958aef05fc39efb5824b6c4fc8f459f59ea58305
DESKTOP-PSR46S4\BS592 <ripon1995dc@gmail.com>
2021-04-02 12:12:33 +0600

    RecylerView added

--- MultiScreenApp/app/src/main/java/com/example/multiscreentest/NumbersActivity.kt
+++ MultiScreenApp/app/src/main/java/com/example/multiscreentest/NumbersActivity.kt
@@ -4,16 +4,37 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.ListView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.multiscreentest.R
 import com.example.multiscreentest.Word
-import com.example.multiscreentest.WordAdapter
+
 import java.util.*
 
 class NumbersActivity : AppCompatActivity() {
+
+    private val words = ArrayList<Word>()
+    private lateinit var customAdapter: CustomAdapterSecond
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_numbers)
-        val words = ArrayList<Word?>()
+
+        prepareItems()
+        prepareAdapter()
+
+        // listview, recyclerview, tab layout, spinner, popup menu, viewpager
+    }
+
+    private fun prepareAdapter()
+    {
+        val recyclerView: RecyclerView = findViewById(R.id.recycler_view)
+        customAdapter = CustomAdapterSecond(words)
+        val layoutManager = LinearLayoutManager(applicationContext)
+        recyclerView.layoutManager = layoutManager
+        recyclerView.adapter = customAdapter
+    }
+
+    private fun prepareItems() {
         words.add(Word("One", "1"))
         words.add(Word("Two", "2"))
         words.add(Word("Three", "3"))
@@ -24,10 +45,36 @@ class NumbersActivity : AppCompatActivity() {
         words.add(Word("Eight", "8"))
         words.add(Word("Nine", "9"))
         words.add(Word("Ten", "10"))
-        val adapter = WordAdapter(this, words)
-        val listView = findViewById<View>(R.id.list) as ListView
-        listView.adapter = adapter
-
-        // listview, recyclerview, tab layout, spinner, popup menu, viewpager
+        words.add(Word("One", "1"))
+        words.add(Word("Two", "2"))
+        words.add(Word("Three", "3"))
+        words.add(Word("Four", "4"))
+        words.add(Word("Five", "5"))
+        words.add(Word("Six", "6"))
+        words.add(Word("Seven", "7"))
+        words.add(Word("Eight", "8"))
+        words.add(Word("Nine", "9"))
+        words.add(Word("Ten", "10"))
+        words.add(Word("One", "1"))
+        words.add(Word("Two", "2"))
+        words.add(Word("Three", "3"))
+        words.add(Word("Four", "4"))
+        words.add(Word("Five", "5"))
+        words.add(Word("Six", "6"))
+        words.add(Word("Seven", "7"))
+        words.add(Word("Eight", "8"))
+        words.add(Word("Nine", "9"))
+        words.add(Word("Ten", "10"))
+        words.add(Word("One", "1"))
+        words.add(Word("Two", "2"))
+        words.add(Word("Three", "3"))
+        words.add(Word("Four", "4"))
+        words.add(Word("Five", "5"))
+        words.add(Word("Six", "6"))
+        words.add(Word("Seven", "7"))
+        words.add(Word("Eight", "8"))
+        words.add(Word("Nine", "9"))
+        words.add(Word("Ten", "10"))
     }
+
 }
