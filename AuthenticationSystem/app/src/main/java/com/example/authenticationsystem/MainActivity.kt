1ac19e36d39f4ac2cf175ccdd9002b2aab876f0b
DESKTOP-PSR46S4\BS592 <ripon1995dc@gmail.com>
2021-07-12 20:30:37 +0600

    google sign in method

--- AuthenticationSystem/app/src/main/java/com/example/authenticationsystem/MainActivity.kt
+++ AuthenticationSystem/app/src/main/java/com/example/authenticationsystem/MainActivity.kt
@@ -1,16 +1,20 @@
 package com.example.authenticationsystem
 
+
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.example.authenticationsystem.emailAuthentication.login.Login
+import com.example.authenticationsystem.googleSignInAuthentication.LogInWithGoogle
 import com.example.authenticationsystem.phoneNumberAuthentication.login.RegisterPhone
+
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var btnEmailAuthentication: Button
     private lateinit var btnPhoneAuthentication: Button
+    private lateinit var btnGoogleAuthentication: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,13 +29,17 @@ class MainActivity : AppCompatActivity() {
         btnPhoneAuthentication.setOnClickListener {
             goToLoginPhoneActivity()
         }
-    }
 
+        btnGoogleAuthentication.setOnClickListener {
+            goToSignInWithGoogleActivity()
+        }
+
+    }
 
     private fun inIt() {
         btnEmailAuthentication = findViewById(R.id.btnEmailAuthentication)
         btnPhoneAuthentication = findViewById(R.id.btnPhoneAuthentication)
-
+        btnGoogleAuthentication = findViewById(R.id.btnGoogleAuthentication)
     }
 
     private fun goToLoginActivity() {
@@ -41,6 +49,11 @@ class MainActivity : AppCompatActivity() {
 
     private fun goToLoginPhoneActivity() {
         val intent = Intent(this, RegisterPhone::class.java)
+        startActivity(intent)
+    }
+
+    private fun goToSignInWithGoogleActivity() {
+        val intent = Intent(this, LogInWithGoogle::class.java)
         startActivity(intent)
     }
 }
--- AuthenticationSystem/app/src/main/java/com/example/authenticationsystem/googleSignInAuthentication/LogInWithGoogle.kt
+++ AuthenticationSystem/app/src/main/java/com/example/authenticationsystem/googleSignInAuthentication/LogInWithGoogle.kt
@@ -1,11 +1,33 @@
 package com.example.authenticationsystem.googleSignInAuthentication
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.authenticationsystem.R
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.common.SignInButton
+
 
 class LogInWithGoogle : AppCompatActivity() {
+    private lateinit var signInButton: SignInButton
+     val RC_SIGN_IN = 101
+    private lateinit var mGoogleSignInClient : GoogleSignInClient
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_log_in_with_google)
+
+        signInButton = findViewById(R.id.btnSignInWithGoogle)
+
+        signInButton.setOnClickListener {
+            signIn()
+        }
     }
+
+    private fun signIn() {
+        val signInIntent: Intent = mGoogleSignInClient.getSignInIntent()
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+    }
+
 }
