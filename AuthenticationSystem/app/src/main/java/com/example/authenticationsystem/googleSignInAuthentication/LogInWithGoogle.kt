4084d538438f8587b401bf3cd2df7f117316d7a5
DESKTOP-PSR46S4\BS592 <ripon1995dc@gmail.com>
2021-07-12 20:28:45 +0600

    done

---
+++ AuthenticationSystem/app/src/main/java/com/example/authenticationsystem/googleSignInAuthentication/LogInWithGoogle.kt
@@ -0,0 +1,11 @@
+package com.example.authenticationsystem.googleSignInAuthentication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LogInWithGoogle : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_log_in_with_google)
+    }
+}
--- Oshudhwala/app/src/main/java/com/example/oshudhwala/account/LoginActivity.kt
+++ Oshudhwala/app/src/main/java/com/example/oshudhwala/account/LoginActivity.kt
@@ -2,14 +2,9 @@ package com.example.oshudhwala.account
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.widget.EditText
-import androidx.core.widget.doOnTextChanged
-import com.example.oshudhwala.R
 import com.example.oshudhwala.base.BaseActivity
 import com.example.oshudhwala.databinding.ActivityLoginBinding
-import com.google.android.material.textfield.TextInputLayout
 
 class LoginActivity : BaseActivity() {
 
@@ -21,7 +16,7 @@ class LoginActivity : BaseActivity() {
         setContentView(binding.root)
 
         binding.btnRegister.setOnClickListener(View.OnClickListener {
-            val intent = Intent(this,RegistrationActivity::class.java)
+            val intent = Intent(this, RegistrationActivity::class.java)
             startActivity(intent)
         })
     }
