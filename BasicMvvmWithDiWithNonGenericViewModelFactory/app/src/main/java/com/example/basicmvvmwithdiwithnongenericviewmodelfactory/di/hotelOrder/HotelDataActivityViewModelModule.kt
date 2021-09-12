f157b1b99d0e44d4c7436056c71ed97834f3a394
DESKTOP-PSR46S4\BS592 <ripon1995dc@gmail.com>
2021-09-12 19:29:46 +0600

    ViewModel key removed

--- BasicMvvmWithDiWithNonGenericViewModelFactory/app/src/main/java/com/example/basicmvvmwithdiwithnongenericviewmodelfactory/di/hotelOrder/HotelDataActivityViewModelModule.kt
+++ BasicMvvmWithDiWithNonGenericViewModelFactory/app/src/main/java/com/example/basicmvvmwithdiwithnongenericviewmodelfactory/di/hotelOrder/HotelDataActivityViewModelModule.kt
@@ -1,17 +1,13 @@
 package com.example.basicmvvmwithdiwithnongenericviewmodelfactory.di.hotelOrder
 
 import androidx.lifecycle.ViewModel
-import com.example.basicmvvmwithdiwithnongenericviewmodelfactory.di.ViewModelKey
 import com.example.basicmvvmwithdiwithnongenericviewmodelfactory.viewModel.hotelOrder.HotelDataActivityViewModel
 import dagger.Binds
 import dagger.Module
-import dagger.multibindings.IntoMap
 
 @Module
 abstract class HotelDataActivityViewModelModule {
-    @Binds
-    @IntoMap
-    @ViewModelKey(HotelDataActivityViewModel::class)
 
+    @Binds
     abstract fun bindViewModel(hotelDataActivityViewModel: HotelDataActivityViewModel): ViewModel
 }
--- BasicMvvmWithDiWithNonGenericViewModelFactory/app/src/main/java/com/example/basicmvvmwithdiwithnongenericviewmodelfactory/di/typeCode/TypeCodeViewModelModule.kt
+++ BasicMvvmWithDiWithNonGenericViewModelFactory/app/src/main/java/com/example/basicmvvmwithdiwithnongenericviewmodelfactory/di/typeCode/TypeCodeViewModelModule.kt
@@ -1,18 +1,12 @@
 package com.example.basicmvvmwithdiwithnongenericviewmodelfactory.di.typeCode
 
 import androidx.lifecycle.ViewModel
-import com.example.basicmvvmwithdiwithnongenericviewmodelfactory.di.ViewModelKey
-import com.example.basicmvvmwithdiwithnongenericviewmodelfactory.viewModel.hotelOrder.HotelDataActivityViewModel
 import com.example.basicmvvmwithdiwithnongenericviewmodelfactory.viewModel.typeCode.TypeCodeViewModel
 import dagger.Binds
 import dagger.Module
-import dagger.multibindings.IntoMap
 
 @Module
 abstract class TypeCodeViewModelModule {
     @Binds
-    @IntoMap
-    @ViewModelKey(TypeCodeViewModel::class)
-
     abstract fun bindViewModel(typeCodeViewModel: TypeCodeViewModel): ViewModel
 }
